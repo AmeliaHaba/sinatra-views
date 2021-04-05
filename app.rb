89c9f6b1f8cb8b9edea34507b456102d6baded71
Amelia Haba <amelia.haba347@gmail.com>
2021-04-05 18:22:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -10,4 +10,19 @@ class App < Sinatra::Base
 		erb :info
 	end 
 
+	get "/hello" do
+		erb :hello
+	end 
+
+	get "/goodbye" do
+		erb :goodbye
+	end 
+
+	get "/date" do
+		erb :date
+	end 
+
+	
+
+
 end
